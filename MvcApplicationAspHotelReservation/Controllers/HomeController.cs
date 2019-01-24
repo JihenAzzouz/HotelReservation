b0325154e72dbf59390e0ee3c42d3d821d828e08
JihenAzzouz <jihenazzouz@gmail.com>
2019-01-24 08:51:04 +0100

    .gitignore fix

---
+++ MvcApplicationAspHotelReservation/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplicationAspHotelReservation.Models;
+
+namespace MvcApplicationAspHotelReservation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private HotelsDBEntities _entities = new HotelsDBEntities();
+
+        public ActionResult Index()
+        {
+            return View(_entities.Hotels.ToList());
+        }
+    }
+}
